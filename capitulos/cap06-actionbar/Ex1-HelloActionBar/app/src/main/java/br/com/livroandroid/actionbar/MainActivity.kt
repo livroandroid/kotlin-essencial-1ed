f14ff122fb940980bff19de8e1c05bbd50c7a5d2
Ricardo Lecheta <rlecheta@livetouch.com.br>
2018-03-18 15:18:31 -0300

    Compilado com Kotlin 1.1.51

--- capitulos/cap06-actionbar/Ex1-HelloActionBar/app/src/main/java/br/com/livroandroid/actionbar/MainActivity.kt
+++ capitulos/cap06-actionbar/Ex1-HelloActionBar/app/src/main/java/br/com/livroandroid/actionbar/MainActivity.kt
@@ -11,7 +11,8 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        actionBar.title = "Olá Action Bar"
+
+        actionBar?.title = "Olá Action Bar"
 
     }
 
