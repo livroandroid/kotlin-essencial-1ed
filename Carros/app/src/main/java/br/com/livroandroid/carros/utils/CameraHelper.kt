a0a373d679b3ae651e33cc99560378d8dd4733eb
Ricardo Lecheta <rlecheta@livetouch.com.br>
2018-03-17 21:50:00 -0300

    Compilado com Kotlin 1.1.51

--- Carros/app/src/main/java/br/com/livroandroid/carros/utils/CameraHelper.kt
+++ Carros/app/src/main/java/br/com/livroandroid/carros/utils/CameraHelper.kt
@@ -13,16 +13,18 @@ import java.io.File
 import java.io.FileOutputStream
 
 class CameraHelper {
+    companion object {
+        private val TAG = "camera"
+    }
 
-    companion object { private val TAG = "camera" }
-
+    // Arquivo pode ser nulo
     var file: File? = null
         private set
 
     // Se girou a tela recupera o estado
-    fun init(savedInstanceState: Bundle?) {
-        if (savedInstanceState != null) {
-            file = savedInstanceState.getSerializable("file") as File
+    fun init(icicle: Bundle?) {
+        if (icicle != null) {
+            file = icicle.getSerializable("file") as File
         }
     }
 
@@ -33,17 +35,19 @@ class CameraHelper {
         }
     }
 
-    // Intent para chamar a câmera
+    // Intent para abrir a câmera
     fun open(context: Context, fileName: String): Intent {
         file = getSdCardFile(context, fileName)
-        Log.d("camera",file.toString())
         val i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        val uri = FileProvider.getUriForFile(context, context.applicationContext.packageName + ".provider", file)
-        i.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        file?.let {
+            Log.d("camera", file.toString())
+            val uri = FileProvider.getUriForFile(context, context.applicationContext.packageName + ".provider", it)
+            i.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        }
         return i
     }
 
-    // Cria o arquivo da foto
+    // Cria o arquivo da foto no diretório privado do app
     fun getSdCardFile(context: Context, fileName: String): File {
         val dir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         if (!dir.exists()) {
@@ -57,11 +61,9 @@ class CameraHelper {
         file?.apply {
             if (exists()) {
                 Log.d(TAG, absolutePath)
-
                 // Resize
                 val bitmap = ImageUtils.resize(this, w, h)
                 Log.d(TAG, "getBitmap w/h: " + bitmap.getWidth() + "/" + bitmap.getHeight())
-
                 return bitmap
             }
         }
