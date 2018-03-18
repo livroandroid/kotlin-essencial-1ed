b0061aea5d71decc1a5d540a35d96c7a7a16e916
Ricardo Lecheta <rlecheta@livetouch.com.br>
2018-03-18 15:21:52 -0300

    Compilado com Kotlin 1.1.51

--- Carros/app/src/main/java/br/com/livroandroid/carros/fragments/MapaFragment.kt
+++ Carros/app/src/main/java/br/com/livroandroid/carros/fragments/MapaFragment.kt
@@ -21,9 +21,9 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapaFragment : BaseFragment(), OnMapReadyCallback {
     // Objeto que controla o Google Maps
     private var map: GoogleMap? = null
-    val carro:Carro by lazy { arguments.getParcelable<Carro>("carro") }
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, b: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.fragment_mapa, container, false)
+    val carro: Carro? by lazy { arguments?.getParcelable<Carro>("carro") }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_mapa, container, false)
         // Inicia o Mapa
         val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -35,32 +35,37 @@ class MapaFragment : BaseFragment(), OnMapReadyCallback {
         // O método onMapReady(map) é chamado quando a inicialização do mapa estiver Ok
         this.map = map
 
-        // Vamos mostrar a localização do usuário apenas para carros com lat/lng=0.
-        if(carro.latitude.toDouble() == 0.0) {
-            // Ativa o botão para mostrar minha localização
-            val ok = PermissionUtils.validate(activity,1,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-            if (ok) {
-                // Somente usa o GPS se a permissão estiver OK.
-                map.isMyLocationEnabled = true
+        // Usa a função apply porque o carro pode ser nulo
+        carro?.apply {
+            // Vamos mostrar a localização do usuário apenas para carros com lat/lng=0.
+            if (latitude.toDouble() == 0.0) {
+                // Ativa o botão para mostrar minha localização
+                val ok = PermissionUtils.validate(activity!!, 1,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                if (ok) {
+                    // Somente usa o GPS se a permissão estiver OK.
+                    map.isMyLocationEnabled = true
+                }
+            } else {
+                // Cria o objeto lat/lng com a coordenada da fábrica
+                val location = LatLng(latitude.toDouble(), longitude.toDouble())
+                // Posiciona o mapa na coordenada da fábrica (zoom = 13)
+                val update = CameraUpdateFactory.newLatLngZoom(location, 13f)
+                map.moveCamera(update)
+                // Marcador no local da fábrica
+                map.addMarker(MarkerOptions()
+                        .title(nome)
+                        .snippet(desc)
+                        .position(location))
             }
-        } else {
-            // Cria o objeto lat/lng com a coordenada da fábrica
-            val location = LatLng(carro.latitude.toDouble(), carro.longitude.toDouble())
-            // Posiciona o mapa na coordenada da fábrica (zoom = 13)
-            val update = CameraUpdateFactory.newLatLngZoom(location, 13f)
-            map.moveCamera(update)
-            // Marcador no local da fábrica
-            map.addMarker(MarkerOptions()
-                    .title(carro.nome)
-                    .snippet(carro.desc)
-                    .position(location))
+
+            // Tipo do mapa: normal, satélite, terreno ou híbrido
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
         }
 
-        // Tipo do mapa: normal, satélite, terreno ou híbrido
-        map.mapType = GoogleMap.MAP_TYPE_NORMAL
+
     }
 
     @SuppressLint("MissingPermission")
